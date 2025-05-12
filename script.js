4940f6b2205c3787385a736b4301076f2666878b
zoelioprojects01 <contact.zoelio@gmail.com>
2025-05-12 20:30:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Zoelio landing page is ready!");
+  
+  // Future JavaScript features, like mobile menu toggle,
+  // can be added here.
+});
